f53fa5b1c9d196ebd2e1880b2dcdf7b3efaf9a15
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-04-27 13:10:11 +0900

    bin: Exclude rel and packer directory

--- bin/ci.sh
+++ bin/ci.sh
@@ -40,7 +40,7 @@ cleanup
 
 vagrant up
 vagrant ssh-config --host build-fuguita > ssh_config
-rsync -avP -e 'ssh -F ssh_config' . build-fuguita:/var/tmp/myfuguita
+rsync -avP --exclude=rel --exclude=packer -e 'ssh -F ssh_config' . build-fuguita:/var/tmp/myfuguita
 ssh -F ssh_config build-fuguita 'sudo mkdir /usr/src /usr/obj || :'
 ssh -F ssh_config build-fuguita 'sudo tar -C /usr/src -zxpf /var/tmp/myfuguita/src.tar.gz; ls -alh /usr/src'
 
